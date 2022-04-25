61d3ff077e12521303eb312fc716ba2e6f25350c
maddarauci <dominicd165@gmail.com>
2022-04-26 01:26:37 +1000

    modified:   Cargo.toml 	modified:   src/main.rs 	new file:   target/.rustc_info.json 	new file:   target/CACHEDIR.TAG 	new file:   target/debug/.cargo-lock 	new file:   target/debug/.fingerprint/blog_os-bced3b8048c8130d/invoked.timestamp 	new file:   target/debug/.fingerprint/blog_os-bced3b8048c8130d/output-bin-blog_os 	new file:   target/debug/deps/blog_os.d 	new file:   target/debug/incremental/blog_os-19mol5jvh5zzs/s-g970l2nqmn-37i4un-working/dep-graph.part.bin 	new file:   target/debug/incremental/blog_os-19mol5jvh5zzs/s-g970l2nqmn-37i4un.lock 	new file:   target/release/.cargo-lock 	new file:   target/release/.fingerprint/blog_os-bced3b8048c8130d/invoked.timestamp 	new file:   target/release/.fingerprint/blog_os-bced3b8048c8130d/output-bin-blog_os 	new file:   target/release/deps/blog_os.d

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 // main.rs 
 
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points.
+//#[lang = "copy"] // attribute that defines it as a language item.
+
 use core::panic::PanicInfo;
 
-#![no_std]
+#[no_mangle] // don't mangle the name of this function.
+pub extern "C" fn _start(0 -> ! {
+    // thus function is the entry point, since the linker looks for a function.
+    // named `_start` by default.
+    loop{}
+})
 
 // this function is call on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
+
+
 
 //fn main() {}
---
+++ target/release/deps/blog_os.d
@@ -0,0 +1,5 @@
+C:\Users\user\desktop\ddk\blog_os\target\release\deps\blog_os.exe: src\main.rs
+
+C:\Users\user\desktop\ddk\blog_os\target\release\deps\blog_os.d: src\main.rs
+
+src\main.rs:
